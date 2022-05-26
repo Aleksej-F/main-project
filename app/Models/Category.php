e2084f611929930180e003e58e6f3b89804b23d0
Aleksej-F <l7933@yandex.ru>
2022-05-26 23:53:45 +0300

    Восстановил что было сделано на 1-5 уроках

---
+++ app/Models/Category.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    //
+    protected $table = "categories";
+    
+    public function getCategories()
+	{
+		return DB::table($this->table)
+			->select(['id', 'title', 'description', 'created_at'])
+			->get();
+	}
+
+    public function getCategory(int $id)
+	{
+       return DB::table($this->table)
+		   ->select(['id', 'title', 'description', 'created_at'])
+		   ->find($id);
+	}
+}
